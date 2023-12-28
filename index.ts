feee869f30d1d547cf0c6e6f821ef3d21c0aa3bd
Pissrith <66420113@kmitl.ac.th>
2023-12-28 07:43:29 +0700

    change to kn

--- index.ts
+++ index.ts
@@ -6,54 +6,116 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 require('dotenv').config()
+app.use(express.json()); 
 
-interface Recycle {
+app.get('/building', async (req: Request, res: Response) => {
+    const building = await prisma.building.findMany(
+        {
+            include: {
+                Restaurant: {
+                include: {
+                    Bill: true,
+                }
+            }
+        }
+        }
+    )
+    res.json({building})
+})
+
+app.get('/building/restaurant', async (req: Request, res: Response) => {
+    const { id } = req.params
+    const building = await prisma.restaurant.findMany(
+        {
+            include: {
+                Bill: true
+            }
+        }   
+    
+    )
+    res.json({building})
+})
+
+app.get('/building/restaurant/:id', async (req: Request, res: Response) => {
+    const { id } = req.params
+    const Bill = await prisma.restaurant.findMany(
+        {
+            where: {
+                id: parseInt(id)
+            },include: {
+                Bill: true
+            }
+        }   
+    
+    )
+    res.json({restaurant: Bill})
+})
+
+app.post('/bill', async (req: Request, res: Response) => {
+    const { Restaurantid, Date, start, end ,mea,sch} = req.body;
+    try {
+      const newBill = await prisma.Bill.create({
+        data: {
+          Restaurantid,
+          Date,
+          start,
+          end,mea,sch
+        }
+      });
+      res.status(201).json(newBill);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while creating the bill.' });
+    }
+  });
+
+  interface Bill {
     id: number;
-    user_id: string;
-    plastic: number;
-    aluminumCans: number;
-    envelope: number;
-    uht: number;
-    glass: number;
-    cardboard: number;
-    otherPaper: number;
-}
+    Date: Date;
+    start: number;
+    end: number;
+    sch:number;
+    mea:number;
+    Restaurantid: number;
+  }
+  app.get('/yearlybill/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const yearlybill = await prisma.Bill.findMany({
+        where: {
+            Restaurantid: parseInt(id)
+        }
+    });
 
-app.get('/recycle', async (req: Request, res: Response) => {
-    try {
-        const sumRecycling = await prisma.recycle.aggregate({
-            _sum: {
-                plastic: true,
-                aluminumCans: true,
-                envelope: true,
-                uht: true,
-                glass: true,
-                cardboard: true,
-                otherPaper: true,
-            },
-        });
-        const sumResult = Object.fromEntries(
-            Object.entries(sumRecycling._sum).map(([key, value]) => [key, value])
-        );
+    const transformedYearlybill = yearlybill.map((bill:Bill) => ({
+        
+        ...bill,
+        Date: new Date(bill.Date).toLocaleString('en-GB', { month: 'short', year: '2-digit' }),
+        คิดตามหน่วยโรงเรียน: (bill.sch * (bill.end - bill.start)).toFixed(2),
+        คิดหน่วยการไฟฟ้า: ((bill.end - bill.start) * bill.mea).toFixed(2) ,
+    }));
 
-        res.json({ sumResult });
-    } catch (error) {
-        console.error('Error fetching sum of recycling items:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    res.json(transformedYearlybill);
 });
 
-app.get('/getProduct', async (req: Request, res: Response) => {
-    try {
-        const response = await prisma.Product.findMany();
-
-        res.json(response);
-    } catch (error) {
-        console.error('Error fetching products:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+app.get('/totalyearlybill', async (req: Request, res: Response) => {
+    const yearlybill = await prisma.Bill.findMany();
+    const groupedYearlybill = yearlybill.reduce((acc: any[], bill: Bill) => {
+        const date = new Date(bill.Date).toLocaleString('en-GB', { month: 'short', year: '2-digit' });
+        let dateIndex = acc.findIndex(item => item.date === date);
+        if (dateIndex === -1) {
+            acc.push({
+                date,
+                คิดตามหน่วยโรงเรียน: parseFloat((bill.sch * (bill.end - bill.start)).toFixed(2)),
+                คิดหน่วยการไฟฟ้า: parseFloat(((bill.end - bill.start) * bill.mea).toFixed(2)),
+            });
+        } else {
+            acc[dateIndex].คิดตามหน่วยโรงเรียน += parseFloat((bill.sch * (bill.end - bill.start)).toFixed(2));
+            acc[dateIndex].คิดหน่วยการไฟฟ้า += parseFloat(((bill.end - bill.start) * bill.mea).toFixed(2));
+        }
+        return acc;
+    }, []);
+    
+    res.json(groupedYearlybill);
 });
-
 app.listen(process.env.PORT || 3001, () => {
     console.log('Example app listening on port', process.env.PORT);
 })
